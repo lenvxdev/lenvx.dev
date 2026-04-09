0a6ede521e1edacd87bd8d412ceca3d5bd76356d
Leo <111296626+lenvxdev@users.noreply.github.com>
2026-04-10 01:57:51 +0300

    :p

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import type { Linter } from "eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,7 +10,7 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig: Linter.Config[] = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
